e570c5ad2dff1b2a732c8f7a3c5c2b515952b148
zionzinhokk <thiagodiaspesquisas@gmail.com>
2026-05-10 19:23:49 -0300

    Commitando pagina home, contas e cardapio

---
+++ js/navigation.js
@@ -0,0 +1,15 @@
+function NavToAccount() {
+    window.location.href = "../html/account-page.html";
+}
+
+function NavToMenu() {
+    window.location.href = "../html/menu-page.html";
+}
+
+function NavToHome() {
+    window.location.href = "../index.html";
+}
+
+function GoBack() {
+    window.history.back();
+}
